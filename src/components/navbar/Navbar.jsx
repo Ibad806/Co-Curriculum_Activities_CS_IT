ad23f89d518b89ea164e5e77b87a0e336ff88f58
Ibad806 <ibadbila029@gmail.com>
2024-11-25 19:09:43 +0500

    .

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "./navbar.css";
+import { CiSearch } from "react-icons/ci";
+import { CgProfile } from "react-icons/cg";
+import { IoNotifications } from "react-icons/io5";
+import { GrFavorite } from "react-icons/gr";
 import { Link } from "react-router-dom";
 import logo from "../media/download.jpeg";
 
 const Navbar = () => {
-  return (
-    <>
+  return (      
+    <>                       
       <nav>
         <div className="navbar-main">
           <div className="logo">
-            {/* <img src={logo} alt="" /> */}
-            <h1>LOGO</h1>
-          </div>
+            {/* <img
+              src={logo}
+            /> */}
+             <h1>LOGO</h1>
+          </div> 
           <div className="nav-menu">
             <ul className="nav-ul">
               <li>
@@ -32,18 +38,25 @@ const Navbar = () => {
               <li>
                 <Link to="/">ABOUT US</Link>
               </li>
-              <img src={logo} className="search-img"  alt="Search" />
+              {/* <img src={logo} className="search-img"  alt="Search" /> */}
+              <CiSearch className="search-icon" />
             </ul>
           </div>
           <div className="nav-right">
             <div className="notifications">
-              <img src={logo} alt="" />
+              <Link to="/">
+                <GrFavorite className="wishlist-icon" />
+              </Link>
             </div>
             <div className="wishlist">
-              <img src={logo} alt="" />
+              <Link to="/">
+                <IoNotifications className="notifications-icon" />
+              </Link>
             </div>
             <div className="profile">
-              <img src={logo} alt="" />
+              <Link to="/">
+                <CgProfile className="profile-icon" />
+              </Link>
             </div>
           </div>
         </div>
@@ -52,4 +65,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Navbar;  
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/navbar/Navbar";
 const Home = () => {
   return (
     <>
-      <Navbar />
+      <Navbar/>
     </>
   );
 };
