9423796df41522c8ce30b044e37d414e168dcfb2
Muhammad Ibad <ibadbilal029@gmail.com>
2024-11-26 21:31:59 +0500

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,7 @@
-<<<<<<< Updated upstream
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
->>>>>>> Stashed changes
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< Updated upstream
 };
-=======
-}
-
->>>>>>> Stashed changes
