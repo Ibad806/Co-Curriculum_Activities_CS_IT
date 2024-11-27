cc131fa1f645dbe1673d087ab49ef057b886490a
Ahmed Nawaz Gondal <nawaz07210721@gmail.com>
2024-11-27 11:00:39 +0500

    ..

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -5,7 +5,7 @@ import { ChevronLeft, ChevronRight } from 'lucide-react'
 
 const images = [
     'https://media.licdn.com/dms/image/v2/D4D0BAQHBgWvpjFKFoA/company-logo_200_200/company-logo_200_200/0/1701371739326?e=1740614400&v=beta&t=oWycWdM29ZnyWV-jBEdYk2d8hNgvZ_8zbhU6SJ7sKeI',
-    '/placeholder.svg?height=400&width=800',
+    'https://th.bing.com/th/id/OIP.hiuKBeYqWj1SftIkeadkkgHaHa?rs=1&pid=ImgDetMain',
     'https://th.bing.com/th/id/R.6cdf26728f97f5780a69ee333df8eb02?rik=lZuMk4y4hej9Jg&pid=ImgRaw&r=0',
     'https://th.bing.com/th/id/OIP.voLVMJX4Uka6I99CIPCv6QHaE8?w=2560&h=1707&rs=1&pid=ImgDetMain',
     'https://live.staticflickr.com/65535/49049007453_c0c4b272ca_o.jpg',
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import NewsSection from "../components/NewsLetter";
 
 const images = [
   'https://media.licdn.com/dms/image/v2/D4D0BAQHBgWvpjFKFoA/company-logo_200_200/company-logo_200_200/0/1701371739326?e=1740614400&v=beta&t=oWycWdM29ZnyWV-jBEdYk2d8hNgvZ_8zbhU6SJ7sKeI',
-  '/placeholder.svg?height=400&width=800',
+  'https://th.bing.com/th/id/OIP.hiuKBeYqWj1SftIkeadkkgHaHa?rs=1&pid=ImgDetMain',
   'https://th.bing.com/th/id/R.6cdf26728f97f5780a69ee333df8eb02?rik=lZuMk4y4hej9Jg&pid=ImgRaw&r=0',
   'https://th.bing.com/th/id/OIP.voLVMJX4Uka6I99CIPCv6QHaE8?w=2560&h=1707&rs=1&pid=ImgDetMain',
   'https://live.staticflickr.com/65535/49049007453_c0c4b272ca_o.jpg',
@@ -19,12 +19,12 @@ const Home = () => {
       <EventSlider heading={"Available Events"} para={"Explore events here"} />
       <main className="flex flex-col items-center justify-center p-24 bg-gray-200">
       <h1 className="text-4xl font-bold mb-8">Amazing Animated Carousel</h1>
-      {/* <ImageSlider images={images} /> */}
+      <ImageSlider images={images} />
       </main>
       <EventSlider heading={"Upcoming Events"} para={"The best events will be soon!"} />
       <main className="flex flex-col items-center justify-center p-24 bg-gray-200">
       <h1 className="text-4xl font-bold mb-8">Amazing Animated Carousel</h1>
-      {/* <ImageSlider images={images} /> */}
+      <ImageSlider images={images} />
       </main>
       <NewsSection/>
     </>
