8387b58e66046899838b8adef1392b2069d662c3
Ahmed Nawaz Gondal <nawaz07210721@gmail.com>
2025-04-18 20:17:58 +0500

    url

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,7 +1,8 @@
 //constants.js
 // http://localhost:4000/
-// https://cac-backend-ibad806s-projects.vercel.app/
-const devUrl = 'https://cac-backend-ibad806s-projects.vercel.app/';
+// https://cac-backend-i957.vercel.app/
+
+const devUrl = 'https://cac-backend-i957.vercel.app/';
 export const BASE_URL = devUrl
 
 export const AppRoutes = {
