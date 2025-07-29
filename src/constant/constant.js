8368bdf18c3e890c47a70ce04b4c0f9bcb9de03f
Muhammad Ibad <ibadbilal029@gmail.com>
2025-07-29 21:46:14 +0500

    Update constant.js

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -2,7 +2,7 @@
 // http://localhost:4000/
 // https://cac-backend-i957.vercel.app/
 
-const devUrl = "https://cac-backend-i957.vercel.app/";
+const devUrl = "https://cac-backend-i957.vercel.app";
 export const BASE_URL = devUrl;
 
 export const AppRoutes = {
